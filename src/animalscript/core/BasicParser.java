deb78257098fbd9cd5c92a5695518276016428a5
Endte <philippschneiderstudent@gmail.com>
2021-01-18 09:24:38 +0100

    added "image" as animalscript command

--- src/animalscript/core/BasicParser.java
+++ src/animalscript/core/BasicParser.java
@@ -247,8 +247,10 @@ public class BasicParser implements AnimalScriptInterface {
 	public static AnimalScriptInterface getHandlerForName(String handlerName) {
 		if (handlerName == null)
 			return null;
-		
+
 		Object o = currentHandlers.get(handlerName);
+		System.out.println("handlerName :"+ handlerName);
+		System.out.println("o: "+ o);
 		if (o != null && o instanceof AnimalScriptInterface)
 			return (AnimalScriptInterface) o;
 
@@ -281,6 +283,7 @@ public class BasicParser implements AnimalScriptInterface {
 
 	public AnimalScriptInterface getHandlerForCommand(String currentCommand) {
 		// AnimalScriptInterface handler = null;
+		System.out.println(currentCommand);
 		String handlerName = registeredKeywords.get(currentCommand);
 		return getHandlerForName(handlerName);
 	}
